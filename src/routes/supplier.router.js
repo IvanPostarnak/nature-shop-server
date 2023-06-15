537470d82a098afd3760512b728f0a89e75f4678
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-06-15 15:07:10 +0300

    feat: add .getAll() method for '/suppliers/all' route

--- src/routes/supplier.router.js
+++ src/routes/supplier.router.js
@@ -1,6 +1,13 @@
 const SupplierRouter = require('express').Router();
 const SupplierController = require('../controller/supplier.controller');
 
+SupplierRouter.route('/all')
+              .get(async (req, res) => {
+                const response = await SupplierController.getAll();
+                res.set('X-Total-Amount', response.count);
+                res.status(response.code).json(response.body);
+              });
+
 SupplierRouter.route('/total_count')
               .get(async (req, res) => {
                 const response = await SupplierController.getTotalCount();
