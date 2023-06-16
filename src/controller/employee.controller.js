a992740cc7038ab75c28f2b81905e451877c7ca8
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-06-16 07:53:58 +0300

    feat: add '/employees/?query=' route and related .getByQuery() method

--- src/controller/employee.controller.js
+++ src/controller/employee.controller.js
@@ -1,9 +1,27 @@
+const filterObjectByValue = require('../helpers/filterObjectByValue');
 const Postgresql = require('../modules/Postgresql');
 const Controller = require('./../entity/Controller');
 
 class EmployeeController extends Controller {
   constructor (database) {
     super(database);
+  }
+
+  async getByQuery(queryObj, infoMod) {
+    let response = {code: 200, body: '', count: 0};
+    const filteredQuery = filterObjectByValue(queryObj);
+    try {
+      response.body = await this.database.getByQuery(filteredQuery, 'employees', infoMod);
+      response.count = response.body.length;
+      if (response.count === 0) {
+        response.code = 404;
+        response.body = `Employees by queries : '${JSON.stringify(filteredQuery)}' were not found`;
+      }
+    } catch (err) {
+      response.code = 500;
+      response.body = err.message;
+    }
+    return response;
   }
 
   async getAll(infoMod) {
