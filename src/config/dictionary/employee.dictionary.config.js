a5afc1be9e171ddd9417b01f4c046adf1dc3c4d2
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-06-16 07:38:17 +0300

    feat: add .getAll() method for '/employees/all' route (accept 'info_mod' query param)

--- src/config/dictionary/employee.dictionary.config.js
+++ src/config/dictionary/employee.dictionary.config.js
@@ -6,6 +6,10 @@ const employeesDictionary = new Map()
 .set(
   'employees basic',
   {name: 'employee_basic_web', column: 'employee_id', type: 'json'}
+)
+.set(
+  'employees full',
+  {name: 'employee_full_web', column: 'employee_id', type: 'json'}
 );
 
 module.exports = employeesDictionary;
--- src/routes/employee.router.js
+++ src/routes/employee.router.js
@@ -1,6 +1,16 @@
 const EmployeeController = require('../controller/employee.controller');
+const infoQuery = require('../middleware/infoQuery');
 const EmployeeRouter = require('express').Router();
 
+EmployeeRouter.route('/all')
+              .get(
+                infoQuery,
+                async (req, res) => {
+                  const response = await EmployeeController.getAll(req.info_mod);
+                  res.set('X-Total-Amount', response.count);
+                  res.status(response.code).json(response.body);
+                }
+              );
 
 EmployeeRouter.route('/total_count')
               .get(async (req, res) => {
