e255db35cdda0f51ed3a09400653c9de1a710985
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-06-16 10:07:03 +0300

    feat: add '/purchases/?query=' route and .getByQuery() method
    Right now we can send request using 5 query params:
    'info_mod', 'id', 'start', 'end' and 'limit'
    
    I think it needs adding 'customer', 'employee', 'date' and 'time' queries

--- src/routes/purchase.router.js
+++ src/routes/purchase.router.js
@@ -1,6 +1,30 @@
 const PurchaseController = require('../controller/purchase.controller');
-const infoQuery = require('../middleware/infoQuery');
 const PurchaseRouter = require('express').Router();
+const endQuery = require('../middleware/endQuery');
+const idQuery = require('../middleware/idQuery');
+const infoQuery = require('../middleware/infoQuery');
+const limitQuery = require('../middleware/limitQuery');
+const startQuery = require('../middleware/startQuery');
+
+PurchaseRouter.route('/')
+              .get(
+                infoQuery,
+                idQuery,
+                startQuery,
+                endQuery,
+                limitQuery,
+                async (req, res) => {
+                  const response = await PurchaseController.getByQuery({
+                    id: req.id,
+                    start: req.start,
+                    end: req.end,
+                    limit: req.limit
+                  }, req.info_mod);
+                  res.set('X-Total-Amount', (await PurchaseController.getTotalCount()).body.total_count);
+                  res.set('X-Current-Amount', response.count);
+                  res.status(response.code).json(response.body);
+                }
+              );
 
 PurchaseRouter.route('/all')
               .get(
