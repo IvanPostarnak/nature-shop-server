25d204f1ede8b756bde2cc6855929abbf2c7d6aa
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-06-16 03:29:04 +0300

    feat: add .getByQuery() shippers' method to accept query

--- src/routes/shipper.router.js
+++ src/routes/shipper.router.js
@@ -1,5 +1,22 @@
 const ShipperRouter = require('express').Router();
 const ShipperController = require('../controller/shipper.controller');
+const idQuery = require('../middleware/idQuery');
+const nameQuery = require('../middleware/nameQuery');
+
+ShipperRouter.route('/')
+             .get(
+               nameQuery,
+               idQuery,
+               async (req, res) => {
+                 const response = await ShipperController.getByQuery({
+                  id: req.id,
+                  name: req.name
+                 });
+                 res.set('X-Total-Amount', (await ShipperController.getTotalCount()).body.total_count);
+                 res.set('X-Current-Amount', response.count);
+                 res.status(response.code).json(response.body);
+               }
+             );
 
 ShipperRouter.route('/all')
              .get(async (req, res) => {
