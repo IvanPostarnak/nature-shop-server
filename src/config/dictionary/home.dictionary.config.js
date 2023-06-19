d38532089f65f4ef19fa76a86fdbbfc2ec65bc6d
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-06-19 10:03:36 +0300

    feat(pages): add '/pages/our_contacts' route in home.dictionary

--- src/config/dictionary/home.dictionary.config.js
+++ src/config/dictionary/home.dictionary.config.js
@@ -6,6 +6,10 @@ const homeDictionary = new Map()
 .set(
   'home about_us',
   {name: 'about_us_view', column: 'about_us_id', type: 'json'}
+)
+.set(
+  'home our_contacts',
+  {name: 'our_contacts_view', column: 'our_contacts_id', type: 'json'}
 );
 
 module.exports = homeDictionary;
