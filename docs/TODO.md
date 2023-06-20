007d03b7c72adfa51e97ce37e3c8dd27134c4387
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-06-20 06:29:13 +0300

    fix(test): change testObj of 'configureTableQuery.test.js'

--- docs/TODO.md
+++ docs/TODO.md
@@ -1,6 +1,7 @@
 # TODOs
 - [ ] To create a common Error() object, to invoke similar error messages easily
 - [ ] To attach testing scripts for whole server
+  - [X] helpers/ functions
 - [ ] To solve comming issues with security: add an area check (check of source of request)
 - [ ] To wrap whole server in the docker container
 - [ ] To create an abstraction for tables, or somehow bound tables' columns and query params. There is a problem with 'configureTableQuery()' method - it is too much specific: I tried to add 'ratingQuery' middleware to 'shops/?rating=' route, but this config function checks only 'rating_5' rating column... so we need to make it less specific
--- tests/unit/helpers/configureTableQuery.test.js
+++ tests/unit/helpers/configureTableQuery.test.js
@@ -1,42 +1,44 @@
 const configureTableQuery = require('../../../src/helpers/configureTableQuery');
 
 const testTable = {name: 'test', column: 'test_id'};
-const testObj1 = {start: 10, end: 20};
-const testObj2 = {author: 7, language: 1};
-const testObj3 = {rating: 4, votes_count: 5};
-const testObj4 = {visited_total: 4};
-const testObj5 = {title: 'title', content: 'content'};
-const testObj6 = {name: 'name', id: 1, method: 'OR', limit: 20};
+const testArr = [
+  {start: 10, end: 20},
+  {author: 7, language: 1},
+  {rating: 4, votes_count: 5},
+  {visited_total: 4},
+  {title: 'title', content: 'content'},
+  {name: 'name', id: 1, method: 'OR', limit: 20}
+];
 
 describe('configureTableQuery(queryObj, table)', () => {
-  test('Returns [query] for queryObj (testObj1) and table (testTable)', () => {
-    expect(configureTableQuery(testObj1, testTable)).toBe(
-      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= ${testObj1.start} AND ${testTable.column} <= ${testObj1.end})`
+  test('Returns [query] for queryObj (testArr[0]) and table (testTable)', () => {
+    expect(configureTableQuery(testArr[0], testTable)).toBe(
+      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= ${testArr[0].start} AND ${testTable.column} <= ${testArr[0].end})`
     );
   });
-  test('Returns [query] for queryObj (testObj2) and table (testTable)', () => {
-    expect(configureTableQuery(testObj2, testTable)).toBe(
-      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= 0) AND (author_id = ${testObj2.author}) AND (language_id = ${testObj2.language})`
+  test('Returns [query] for queryObj (testArr[1]) and table (testTable)', () => {
+    expect(configureTableQuery(testArr[1], testTable)).toBe(
+      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= 0) AND (author_id = ${testArr[1].author}) AND (language_id = ${testArr[1].language})`
     );
   });
-  test('Returns [query] for queryObj (testObj3) and table (testTable)', () => {
-    expect(configureTableQuery(testObj3, testTable)).toBe(
-      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= 0) AND (rating >= ${testObj3.rating}) AND (votes_count >= ${testObj3.votes_count})`
+  test('Returns [query] for queryObj (testArr[2]) and table (testTable)', () => {
+    expect(configureTableQuery(testArr[2], testTable)).toBe(
+      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= 0) AND (rating >= ${testArr[2].rating}) AND (votes_count >= ${testArr[2].votes_count})`
     );
   });
-  test('Returns [query] for queryObj (testObj4) and table (testTable)', () => {
-    expect(configureTableQuery(testObj4, testTable)).toBe(
-      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= 0) AND (visited_total >= ${testObj4.visited_total})`
+  test('Returns [query] for queryObj (testArr[3]) and table (testTable)', () => {
+    expect(configureTableQuery(testArr[3], testTable)).toBe(
+      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= 0) AND (visited_total >= ${testArr[3].visited_total})`
     );
   });
-  test('Returns [query] for queryObj (testObj5) and table (testTable)', () => {
-    expect(configureTableQuery(testObj5, testTable)).toBe(
-      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= 0) AND (title ILIKE '%${testObj5.title}%') AND (content ILIKE '%${testObj5.content}%')`
+  test('Returns [query] for queryObj (testArr[4]) and table (testTable)', () => {
+    expect(configureTableQuery(testArr[4], testTable)).toBe(
+      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= 0) AND (title ILIKE '%${testArr[4].title}%') AND (content ILIKE '%${testArr[4].content}%')`
     );
   });
-  test('Returns [query] for queryObj (testObj6) and table (testTable)', () => {
-    expect(configureTableQuery(testObj6, testTable)).toBe(
-      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= 0) ${testObj6.method} (name ILIKE '%${testObj6.name}%') ${testObj6.method} (${testTable.column} = ${testObj6.id}) LIMIT ${testObj6.limit}`
+  test('Returns [query] for queryObj (testArr[5]) and table (testTable)', () => {
+    expect(configureTableQuery(testArr[5], testTable)).toBe(
+      `SELECT * FROM ${testTable.name} WHERE (${testTable.column} >= 0) ${testArr[5].method} (name ILIKE '%${testArr[5].name}%') ${testArr[5].method} (${testTable.column} = ${testArr[5].id}) LIMIT ${testArr[5].limit}`
     );
   });
 });
